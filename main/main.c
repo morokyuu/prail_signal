d97150bd0cca69f66a8540bd610f2808bab8c80a
nano <nano@example.com>
2023-01-02 02:01:32 +0900

    pwm duty cycle

--- main/main.c
+++ main/main.c
@@ -3,14 +3,16 @@
 #include "hardware/pwm.h"
 
 int main() {
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_set_function(0,LED_PIN);
-    //gpio_set_function(1,GPIO_FUNC_PWM);
+    //const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    //gpio_set_function(0,LED_PIN);
+    gpio_set_function(0,GPIO_FUNC_PWM);
+    gpio_set_function(1,GPIO_FUNC_PWM);
 
-    uint slice_num = pwm_gpio_to_slice_num(LED_PIN);
+    uint slice_num = pwm_gpio_to_slice_num(0);
 
-    pwm_set_wrap(slice_num,3);
-    pwm_set_chan_level(slice_num, PWM_CHAN_B, 0);
+    pwm_set_wrap(slice_num,10);
+    pwm_set_chan_level(slice_num, PWM_CHAN_A, 2);
+    pwm_set_chan_level(slice_num, PWM_CHAN_B, 4);
 
     pwm_set_enabled(slice_num, true);
 
