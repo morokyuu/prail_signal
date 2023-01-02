5f414abd95636c6e437f37d13b09fbb8567a7953
nano <nano@example.com>
2023-01-03 00:18:48 +0900

    38kHz adjusted

--- main/main.c
+++ main/main.c
@@ -3,21 +3,22 @@
 #include "hardware/pwm.h"
 
 int main() {
-    //const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    //gpio_set_function(0,LED_PIN);
     gpio_set_function(0,GPIO_FUNC_PWM);
     gpio_set_function(1,GPIO_FUNC_PWM);
 
     uint slice_num = pwm_gpio_to_slice_num(0);
-    pwm_set_wrap(slice_num,10);
-    pwm_config config = pwm_get_default_config();
-    pwm_config_set_clkdiv(&config,2.f);
-    //pwm_config_set_clkdiv_int_frac(&config,255,15);
-    pwm_init(slice_num,&config,true);
-    pwm_set_gpio_level(0, 65534/2);
-    pwm_set_gpio_level(1, 65534/4);
 
-    int duty=0;
+    pwm_config config;
+    pwm_config_set_phase_correct(&config, false);
+    pwm_config_set_clkdiv_int(&config, 1);
+    pwm_config_set_clkdiv_mode(&config, PWM_DIV_FREE_RUNNING);
+    pwm_config_set_output_polarity(&config, false, false);
+    pwm_config_set_wrap(&config, 3300);
+
+    pwm_init(slice_num,&config,true);
+    pwm_set_gpio_level(0, (int)(3300 * 0.5));
+    pwm_set_gpio_level(1, (int)(3300 * 0.8));
+
     for(;;){
         tight_loop_contents();
     }
