fc01fa25b54fe24ce60f27a9aa4cbfda025619e3
nano <nano@example.com>
2023-01-02 10:11:54 +0900

    fade LED

--- main/main.c
+++ main/main.c
@@ -9,12 +9,19 @@ int main() {
     gpio_set_function(1,GPIO_FUNC_PWM);
 
     uint slice_num = pwm_gpio_to_slice_num(0);
-
     pwm_set_wrap(slice_num,10);
-    pwm_set_chan_level(slice_num, PWM_CHAN_A, 2);
-    pwm_set_chan_level(slice_num, PWM_CHAN_B, 4);
 
-    pwm_set_enabled(slice_num, true);
+    int duty=0;
+    for(;;){
+        pwm_set_chan_level(slice_num, PWM_CHAN_A, duty);
+        pwm_set_chan_level(slice_num, PWM_CHAN_B, 4);
+        pwm_set_enabled(slice_num, true);
+        sleep_ms(400);
+        duty++;
+        if(duty > 10){
+            duty = 0;
+        }
+    }
 
 
 }
